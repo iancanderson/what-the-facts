4c02bec9bca6235703ad2cf5e9ace7cfbc9b63ea
Ian C. Anderson <ian@iancanderson.com>
2016-11-06 17:46:42 -0500

    Multiple choice question model

--- Main.elm
+++ Main.elm
@@ -1,47 +1,104 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, p, text)
 import Html.App as App
 import Html.Events exposing (onClick)
 
 
 main =
-  App.beginnerProgram { model = model, view = view, update = update }
+    App.beginnerProgram { model = model, view = view, update = update }
+
 
 
 -- MODEL
 
-type alias Answer = String
-type alias Model = Answer
+
+type alias Answer =
+    String
+
+
+type alias Option =
+    { body : String
+    , correct : Bool
+    }
+
+
+type alias Question =
+    { body : String
+    , options : List Option
+    }
+
+
+type alias Model =
+    { lastAnswer : Answer
+    , questions : List Question
+    , currentQuestion : Question
+    }
+
+
+firstQuestion : Question
+firstQuestion =
+    Question "Where is Aleppo?"
+        [ Option "Iraq" False
+        , Option "Syria" True
+        , Option "Afghanistan" False
+        ]
+
+
+allQuestions : List Question
+allQuestions =
+    [ firstQuestion ]
+
 
 model : Model
 model =
-  ""
+    { lastAnswer = ""
+    , questions = allQuestions
+    , currentQuestion = firstQuestion
+    }
+
+
 
 -- UPDATE
 
-type Msg = SubmitAnswer Answer
+
+type Msg
+    = SubmitAnswer Answer
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    SubmitAnswer answer ->
-      answer
+    case msg of
+        SubmitAnswer answer ->
+            { model | lastAnswer = answer }
+
+
 
 -- VIEW
 
+
 scoreView : Model -> Html Msg
 scoreView model =
-  p [] [ text <| "Last answer: " ++ model ]
+    p []
+        [ text <| "Last answer: " ++ model.lastAnswer ]
+
+
+optionView : Option -> Html Msg
+optionView option =
+    button [ onClick <| SubmitAnswer option.body ] [ text option.body ]
+
+
+questionView : Model -> Html Msg
+questionView model =
+    div []
+        [ p [] [ text model.currentQuestion.body ]
+        , div [] <| List.map optionView model.currentQuestion.options
+        ]
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [
-      scoreView model,
-      p []
-        [ text "Where is Aleppo?" ],
-      div []
-        [ button [ onClick <| SubmitAnswer "Iraq" ] [ text "Iraq" ]
-        , button [ onClick <| SubmitAnswer "Syria" ] [ text "Syria" ]
-        , button [ onClick <| SubmitAnswer "Afghanistan" ] [ text "Afghanistan" ]
+    div []
+        [ scoreView model
+        , questionView model
         ]
-    ]
